ba9a7e21efd2eb3eb71124bcba22b38b9ddcdc59
Atyub Ahmad <97192723+atyubahmad@users.noreply.github.com>
2025-10-02 13:39:18 -0400

    Time: 0 ms (100%), Space: 18 MB (17.77%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,13 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        sRev = s[::-1]
-        wordL = 0
-        for i in range(len(sRev)):
-            if sRev[i] != " ":
-                wordL += 1
-                if i + 1 != len(sRev) and sRev[i + 1] == " ":
-                    break
-          
-
-
-        return wordL
+        string = [w for w in s.split(" ") if w.strip()]
+        return(len(string[-1]))
+        
